37fbcb12662689b2f514137b55732e43de6d518a
Ivan <risticaivan@gmail.com>
2024-07-28 19:40:13 +0200

    small fix

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,13 +2,11 @@ import React from "react";
 import Keyboard from "../components/Keyboard";
 import Output from "../components/OutputArea";
 import "@/styles/globals.css";
-import { KeyboardProvider } from "../helpers/KeyboardContext"; // Import the KeyboardProvider
+import { KeyboardProvider } from "../helpers/KeyboardContext";
 
 const App = () => {
   return (
     <KeyboardProvider>
-      {" "}
-      {/* Wrap the App component with KeyboardProvider */}
       <div className="app">
         <Keyboard />
         <Output />
